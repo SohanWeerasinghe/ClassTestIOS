19eb726730578d54db4c2ef8d1beea50dd931774
Sohan <codse223f-124@student.nibm.lk>
2026-07-09 15:29:13 +0530

    Game session logic update - core location error fix

--- Class_Task/Models/GameSession.swift
+++ Class_Task/Models/GameSession.swift
@@ -1,5 +1,6 @@
 import Foundation
 import CoreLocation
+import Combine
 
 struct GameSession: Identifiable, Codable {
     let id: UUID
@@ -8,6 +9,38 @@ struct GameSession: Identifiable, Codable {
     let date: Date
     let latitude: Double
     let longitude: Double
+    let accuracy: Double
+    
+    init(id: UUID, gameName: String, score: Int, date: Date, latitude: Double, longitude: Double, accuracy: Double) {
+        self.id = id
+        self.gameName = gameName
+        self.score = score
+        self.date = date
+        self.latitude = latitude
+        self.longitude = longitude
+        self.accuracy = accuracy
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case gameName
+        case score
+        case date
+        case latitude
+        case longitude
+        case accuracy
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
+        gameName = try container.decode(String.self, forKey: .gameName)
+        score = try container.decode(Int.self, forKey: .score)
+        date = try container.decode(Date.self, forKey: .date)
+        latitude = try container.decode(Double.self, forKey: .latitude)
+        longitude = try container.decode(Double.self, forKey: .longitude)
+        accuracy = try container.decodeIfPresent(Double.self, forKey: .accuracy) ?? 0
+    }
 }
 
 class GameSessionStore: ObservableObject {
@@ -22,18 +55,26 @@ class GameSessionStore: ObservableObject {
     }
     
     func addSession(gameName: String, score: Int, location: CLLocation?) {
-        let coordinate = location?.coordinate ?? CLLocationCoordinate2D(latitude: 6.9271, longitude: 79.8612)
+        let hasUsableLocation = location.map {
+            $0.horizontalAccuracy > 0 && $0.horizontalAccuracy <= 100
+        } ?? false
         
         let newSession = GameSession(
             id: UUID(),
             gameName: gameName,
             score: score,
             date: Date(),
-            latitude: coordinate.latitude,
-            longitude: coordinate.longitude
+            latitude: hasUsableLocation ? location?.coordinate.latitude ?? 0 : 0,
+            longitude: hasUsableLocation ? location?.coordinate.longitude ?? 0 : 0,
+            accuracy: hasUsableLocation ? location?.horizontalAccuracy ?? 0 : 0
         )
         
         sessions.append(newSession)
+        saveSessions()
+    }
+    
+    func clearSessions() {
+        sessions.removeAll()
         saveSessions()
     }
     
